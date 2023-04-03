61047168ae71a010777197517e8729de610cd0e5
Sebastian Bøe <sebastian.boe@nordicsemi.no>
2023-04-03 11:49:17 +0200

    Add disabled ppi_trace instrumentation
    Signed-off-by: Sebastian Bøe <sebastian.boe@nordicsemi.no>

--- tests/subsys/bootloader/bug/custom_spm_app/application/src/main.c
+++ tests/subsys/bootloader/bug/custom_spm_app/application/src/main.c
@@ -3,18 +3,11 @@
 #include <stdio.h>
 #include <secure_services.h>
 
-bool rcrOtpRead_nse
-(
-  uint8_t  xField,
-  uint8_t*      xpDest,
-  size_t*       xpNumBytes
-);
-bool rcrOtpRead
-(
-  uint8_t  xField,
-  uint8_t*      xpDest,
-  size_t*       xpNumBytes
-)
+#include <debug/ppi_trace.h>
+
+bool rcrOtpRead_nse(uint8_t xField, uint8_t *xpDest, size_t *xpNumBytes);
+
+bool rcrOtpRead(uint8_t xField, uint8_t *xpDest, size_t *xpNumBytes)
 {
   bool retVal = false;
   unsigned int irqKey = 0u;
@@ -33,8 +26,30 @@ static void my_timer_handler(struct k_timer *dummy)
 
 static K_TIMER_DEFINE(my_timer, my_timer_handler, NULL);
 
+void config(uint32_t pin, uint32_t event) {
+	void * handle_trace = ppi_trace_config(pin, event);
+
+	if(handle_trace == NULL) {
+		printk("Failed");
+	a: goto a;
+	}
+
+	ppi_trace_enable(handle_trace);
+}
+
+#define TIMER_PIN 0
+#define EGU_PIN 1
+
+void trace_enable(void) {
+	/* config(TIMER_PIN, (uint32_t)(&NRF_RTC1_NS->EVENTS_COMPARE[0])); */
+	/* config(EGU_PIN, (uint32_t)(&NRF_EGU0_NS->EVENTS_TRIGGERED[0])); */
+}
+
 void main(void){
     printk("Hello World! %s\n", CONFIG_BOARD);
+
+	trace_enable();
+
     k_timer_start(&my_timer, K_USEC(100), K_USEC(300));
     for (uint32_t idx = 0u; idx < 0xFFFFFF; idx++)
     {
