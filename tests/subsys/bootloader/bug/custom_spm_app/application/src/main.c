082a52a5575d134d6effc990d45345a5ad0ef035
Sebastian Bøe <sebastian.boe@nordicsemi.no>
2023-04-03 15:43:31 +0200

    misc changes
    Signed-off-by: Sebastian Bøe <sebastian.boe@nordicsemi.no>

--- tests/subsys/bootloader/bug/custom_spm_app/application/src/main.c
+++ tests/subsys/bootloader/bug/custom_spm_app/application/src/main.c
@@ -3,7 +3,17 @@
 #include <stdio.h>
 #include <secure_services.h>
 
+#include <hal/nrf_rtc.h>
+#include <hal/nrf_egu.h>
+
+
 #include <debug/ppi_trace.h>
+
+
+#define TIMER_PIN 10
+#define EGU0_PIN 11
+#define EGU1_PIN 12
+
 
 bool rcrOtpRead_nse(uint8_t xField, uint8_t *xpDest, size_t *xpNumBytes);
 
@@ -26,23 +36,34 @@ static void my_timer_handler(struct k_timer *dummy)
 
 static K_TIMER_DEFINE(my_timer, my_timer_handler, NULL);
 
-void config(uint32_t pin, uint32_t event) {
-	void * handle_trace = ppi_trace_config(pin, event);
+static void conf(uint32_t pin, uint32_t evt)
+{
+	void *handle;
 
-	if(handle_trace == NULL) {
-		printk("Failed");
-	a: goto a;
-	}
+	handle = ppi_trace_config(pin, evt);
+	__ASSERT(handle != NULL,
+		"Failed to initialize trace pin, no PPI or GPIOTE resources?");
 
-	ppi_trace_enable(handle_trace);
+	ppi_trace_enable(handle);
 }
 
-#define TIMER_PIN 0
-#define EGU_PIN 1
-
 void trace_enable(void) {
-	/* config(TIMER_PIN, (uint32_t)(&NRF_RTC1_NS->EVENTS_COMPARE[0])); */
-	/* config(EGU_PIN, (uint32_t)(&NRF_EGU0_NS->EVENTS_TRIGGERED[0])); */
+	conf(TIMER_PIN,
+		 nrf_rtc_event_address_get(NRF_RTC1_NS, NRF_RTC_EVENT_COMPARE_0));
+
+	NRF_P0->DIRSET |= 1 << EGU0_PIN;
+	NRF_P0->DIRSET |= 1 << EGU1_PIN;
+	//	NRF_P0->DIRSET |= 1 << 13;
+
+	NRF_P0->OUTSET |= 1 << EGU0_PIN;
+	for(volatile int i = 0; i < 1000; i++);
+	NRF_P0->OUTCLR |= 1 << EGU0_PIN;
+
+	NRF_P0->OUTSET |= 1 << EGU1_PIN;
+	for(volatile int i = 0; i < 1000; i++);
+	NRF_P0->OUTCLR |= 1 << EGU1_PIN;
+
+		__NOP(); __NOP(); __NOP(); __NOP();
 }
 
 void main(void){
